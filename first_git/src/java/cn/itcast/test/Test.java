e491b9d145d2da187af1b9df3bcacedd94e98d65
admin <1784368001@qq.com>
2019-02-21 10:24:45 +0800

    11111111111111

---
+++ first_git/src/java/cn/itcast/test/Test.java
@@ -0,0 +1,18 @@
+package java.cn.itcast.test;
+
+/**
+ * @author 黑马程序员:TTC130
+ * @Company http://www.ithiema.com
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+
+
+    }
+}
